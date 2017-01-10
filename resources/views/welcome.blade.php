d989e4ba82af10fd4f4dd58f884f50404b6b8dac
crynobone <crynobone@gmail.com>
2017-01-10 15:11:15 +0800

    Update welcome page.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>KATSANA&trade; API Explorer</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -74,16 +74,13 @@
             @endif
 
             <div class="content">
+                <img src="{{ asset('images/katsana.png') }}" width="120" height="120">
                 <div class="title m-b-md">
-                    Laravel
+                    API Explorer
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="#">Documentation</a>
                 </div>
             </div>
         </div>
